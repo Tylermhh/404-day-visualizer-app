739e37faf3d9f326656814717e96e062b22d4004
Xavier Garcia <xmangarcia17@gmail.com>
2024-02-09 00:26:45 -0800

    feat: create mongoose schemas

---
+++ packages/express-backend/Model/task.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const TaskSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  userID: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  description: { type: String, required: false },
+  category: { type: String, required: true },
+  priority: { type: String, required: false },
+  datesUpdated: [
+    {
+      date: { type: Date, required: true },
+      time: { type: Number, required: true },
+    },
+  ],
+  done: { type: Boolean, required: true },
+});
+
+const Task = mongoose.model("Task", TaskSchema);
+
+export default Task;
