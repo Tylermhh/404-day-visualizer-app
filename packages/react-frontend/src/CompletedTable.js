63d146b2d2e8d53d152c5a352849e67503308e1d
Xavier Garcia <xmangarcia17@gmail.com>
2024-02-21 16:11:09 -0800

    refactor: move files from inner react-frontend dir to first level in packages

---
+++ packages/react-frontend/src/CompletedTable.js
@@ -0,0 +1,55 @@
+import React from "react";
+import ToggleSwitch from "react-toggle-switch";
+
+function TableHeader() {
+    return (
+      <thead>
+        <tr>
+          <th>Item Name</th>
+          <th>Category</th>
+          <th>Completed</th>
+        </tr>
+      </thead>
+    );
+  }
+  
+function TableBody(props) {
+    const handleToggle = (index) => {
+      props.toggleTask(index);
+    };
+
+    const rows = props.taskData.map((row, index) => {
+        return (
+            <tr key = {index}>
+                <td>{row.name}</td>
+                <td>{row.category}</td>
+                <td>
+                    <ToggleSwitch
+                      onClick={() => handleToggle(index)}
+                      on={row.completed}
+                    />
+                </td>
+            </tr>
+        );
+    });
+    return (
+        <tbody>
+            {rows}
+        </tbody>
+    );
+}
+
+function CompletedTable(props) {
+
+    return (
+        <table>
+        <TableHeader />
+        <TableBody 
+            taskData = {props.taskData}
+            toggleTask={props.toggleTask}
+        />
+        </table>
+    );
+}
+
+export default CompletedTable;
