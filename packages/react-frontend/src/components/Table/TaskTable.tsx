2df9b78b44eaa6014b1f9f8cfee694403dd6e613
Min Hset <hlaing@calpoly.edu>
2024-02-27 15:58:24 -0800

    just copied home table for now with minor modificatios. need to add more

--- packages/react-frontend/src/components/Table/TaskTable.tsx
+++ packages/react-frontend/src/components/Table/TaskTable.tsx
@@ -3,6 +3,24 @@ import styles from "./Table.module.css";
 import { Button, Col, Form, Row, Table} from 'react-bootstrap';
 import {Task} from "./../../types/types";
 
+const TableTitle: React.FC<{}> = () => {
+    return (
+      <div>
+        <Row>
+          <Col sm={11}>
+            <h2>
+              Todo List (Coming Up)
+            </h2>
+          </Col>
+          <Col sm={1}>
+            <Button variant="outline-primary">
+              +
+            </Button>
+          </Col>
+        </Row>
+      </div>
+    )
+  }
 
 const TableHeader: React.FC<{}> = () => {
     return (
@@ -21,6 +39,49 @@ const TableHeader: React.FC<{}> = () => {
       </div>
     )
   }
+
+  
+const TableBody: React.FC<{taskData : Task[]}> = ({taskData}) => {
+    const rows = taskData.map((row: any, index: any) => {
+        return (
+          <tr>
+            <td>{row.item_name}</td>
+            <td>{row.category}</td>
+            <td>
+              <Form>
+                <Form.Check
+                  type={'checkbox'}
+                  id={'compelted'}
+                />
+              </Form>
+            </td>
+            <td>
+            <Button>
+              Delete
+            </Button>
+            </td>
+          </tr>
+        );
+    });
+
+    return (
+        <div className = {styles['table-responsive']}>
+          <Table striped hover>
+            <thead>
+              <tr>
+                <th>Item Name</th>
+                <th>Category</th>
+                <th>Completed</th>
+                <th>Remove</th>
+              </tr>
+            </thead>
+            <tbody>
+              { rows }
+            </tbody>
+          </Table>
+        </div>
+      );
+}
 
   const TaskTable: React.FC<{taskData : Task[]}> = (input) => {
     return (
