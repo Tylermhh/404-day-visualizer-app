14463624004f34d5cf6266bdd88dfa45d2283efe
Min Hset <hlaing@calpoly.edu>
2024-02-28 19:47:16 -0800

    added some new task obj defs for completed tasks (temp)

--- packages/react-frontend/src/components/TaskPage.tsx
+++ packages/react-frontend/src/components/TaskPage.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Navbar from "./Nav/Nav";
- 
+
+import { CompletedTasks } from "./../types/types"; 
+
+
+let tempTasks: CompletedTasks[] = [
+  { item_name: "Make Frontend", description: "Add pie chart", start_date:  new Date("2024-01-16"), complete_date:  new Date("2024-01-21"), category: "Academics", time_spent: "7hrs"},
+  { item_name: "Make Frontend", description: "Add pie chart", start_date:  new Date("2024-01-16"), complete_date:  new Date("2024-01-21"), category: "Academics", time_spent: "7hrs"},
+  { item_name: "Make Frontend", description: "Add pie chart", start_date:  new Date("2024-01-16"), complete_date:  new Date("2024-01-21"), category: "Academics", time_spent: "7hrs"},
+  { item_name: "Make Frontend", description: "Add pie chart", start_date:  new Date("2024-01-16"), complete_date:  new Date("2024-01-21"), category: "Academics", time_spent: "7hrs"},
+  { item_name: "Make Frontend", description: "Add pie chart", start_date:  new Date("2024-01-16"), complete_date:  new Date("2024-01-21"), category: "Academics", time_spent: "7hrs"},
+  { item_name: "Make Frontend", description: "Add pie chart", start_date:  new Date("2024-01-16"), complete_date:  new Date("2024-01-21"), category: "Academics", time_spent: "7hrs"},
+  { item_name: "Make Frontend", description: "Add pie chart", start_date:  new Date("2024-01-16"), complete_date:  new Date("2024-01-21"), category: "Academics", time_spent: "7hrs"},
+];
+
 function Task() {
   return (
     <div className="App">
--- packages/react-frontend/src/types/types.tsx
+++ packages/react-frontend/src/types/types.tsx
@@ -9,3 +9,21 @@ export interface CategoryProgress {
     hours: number;
     color: string;
 }
+
+export interface TaskDetails {
+    item_name: string;
+    description: string;
+    priority: string;
+    category: string;
+    completed: boolean;
+    deadline: Date;
+}
+
+export interface CompletedTasks {
+    item_name: string;
+    description: string;
+    start_date: Date;
+    complete_date: Date;
+    category: string;
+    time_spent: string;
+}
