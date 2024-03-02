2deea5d54c310d25945e3f3788e397e87c0969c7
Tylermhh <tyler.hlaing@gmail.com>
2024-03-01 16:17:15 -0800

    added href random link

--- packages/react-frontend/src/components/Nav/LoginNav.tsx
+++ packages/react-frontend/src/components/Nav/LoginNav.tsx
@@ -34,7 +34,7 @@ const LandingNav: React.FC<{}> = () => {
         <Navbar expand="lg" className = {styles.navbar}>
             <Container>
                 <div className = {styles['logo-container']}>
-                    <a className="navbar-brand" href="javascript:void(0)">
+                    <a className="navbar-brand" href="https://www.google.com/">
                         TaskCraft
                     </a>
                 </div>
