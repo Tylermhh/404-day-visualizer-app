dbf2aa87906e23f25d3130bf9ab7536779eabe46
ChocoKnight <ktan30@calpoly.edu>
2024-03-07 16:08:13 -0800

    username and password forms  on sign up and ogin page

--- packages/react-frontend/src/components/AuthenticationPage.tsx
+++ packages/react-frontend/src/components/AuthenticationPage.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import MainNav from "./Nav/MainNav";
+import { Button, Container, Col, Row, Form } from "react-bootstrap";
 
 function Main() {
     const navigate = useNavigate();
@@ -20,15 +21,35 @@ function Main() {
             <MainNav
                 page = { "Auth" }
             />
-            <header className="App-header">
-                <p>Auth Page</p>
-                <button onClick={gotToHomePageComp}>
-                    go to Home Page
-                </button>
-                <button onClick={goToTaskPageComp}>
-                    go to Task Page
-                </button>
-            </header>
+            <Container>
+                <Row>
+                    <Col sm={3}>
+                    </Col>
+                    <Col sm={6}>
+                        <Form>
+                            <Form.Group className="mb-3" controlId="signup-username">
+                                <Form.Label>Username</Form.Label>
+                                    <Form.Control   
+                                        type="username"
+                                        placeholder="Username"
+                                        autoFocus
+                                    />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="signup-password">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control 
+                                    type="password"
+                                    placeholder="Password"/>
+                                <Form.Text id="passwordHelpBlock" muted>
+                                    Your password must be 8-20 characters long.
+                                </Form.Text>
+                            </Form.Group>
+                        </Form>
+                    </Col>
+                    <Col sm={3}>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
