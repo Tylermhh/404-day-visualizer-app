fcd38935af254f3d52c3d6cb421efcc90c6df7b3
ChocoKnight <ktan30@calpoly.edu>
2024-03-08 19:58:09 -0800

    EsLint Fixes and adding formatted forms and button onto login page

--- packages/react-frontend/src/components/SignUpPage.tsx
+++ packages/react-frontend/src/components/SignUpPage.tsx
@@ -1,50 +1,55 @@
-import { useNavigate } from 'react-router-dom';
 import MainNav from "./Nav/MainNav";
-import { Button, Container, Col, Row, Form } from "react-bootstrap";
+import { Button, Container, Col, Row, Form, Stack} from "react-bootstrap";
 
-
-function Main() {
-    const navigate = useNavigate();
- 
+function Main() { 
     return (
         <div className="App">
             <MainNav
                 page = { "SignUp" }
             />
-            <Container>
-                <Row>
-                    <Col sm={3}>
-                    </Col>
-                    <Col sm={6}>
-                        <Form>
-                            <Form.Group className="mb-3" controlId="signup-username">
-                                <Form.Label>Username</Form.Label>
-                                    <Form.Control   
-                                        type="username"
-                                        placeholder="Username"
-                                        autoFocus
-                                    />
-                            </Form.Group>
-                            <Form.Group className="mb-3" controlId="signup-password">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control 
-                                    type="password"
-                                    placeholder="Password"/>
-                                <Form.Text id="passwordHelpBlock" muted>
-                                    Your password must be 8-20 characters long.
-                                </Form.Text>
-                            </Form.Group>
-                        </Form>
-                    </Col>
-                    <Col sm={3}>
-                    </Col>
-                </Row>
-                <Row>
-                    <Button variant="primary" size="lg">
-                        Block level button
-                    </Button>
-                </Row>
-            </Container>
+            <Stack gap = {5}>
+                <Container />
+                <Container>
+                    <Row>
+                        <Col sm={3}>
+                        </Col>
+                        <Col sm={6}>
+                            <Form>
+                                <Form.Group className="mb-3" controlId="signup-username">
+                                    <Form.Label>Username</Form.Label>
+                                        <Form.Control   
+                                            type="username"
+                                            placeholder="Username"
+                                            autoFocus
+                                        />
+                                </Form.Group>
+                                <Form.Group className="mb-3" controlId="signup-password">
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control 
+                                        type="password"
+                                        placeholder="Password"/>
+                                    <Form.Text id="passwordHelpBlock" muted>
+                                        Your password must be 8-20 characters long.
+                                    </Form.Text>
+                                </Form.Group>
+                            </Form>
+                        </Col>
+                        <Col sm={3}>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col sm={5}>
+                        </Col>
+                        <Col sm={6}>
+                            <Button variant="primary" size="lg">
+                                Block level button
+                            </Button>
+                        </Col>
+                        <Col sm={1}>
+                        </Col>
+                    </Row>
+                </Container>
+            </Stack>
         </div>
     );
 }
