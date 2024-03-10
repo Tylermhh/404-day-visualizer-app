d73144539497cc3dc8e55f1df5d65b962b234ffb
Tylermhh <tyler.hlaing@gmail.com>
2024-03-09 16:11:44 -0800

    tried changing mongo_url to take literal string. (not secure at all)

--- packages/express-backend/db.js
+++ packages/express-backend/db.js
@@ -3,12 +3,13 @@ import * as dotenv from "dotenv"
 dotenv.config()
 
 const url = process.env.MONGO_URI
+const urlString = "mongodb+srv://404manager:S2rypinCTmw63y6T@cluster404.tvc0s3w.mongodb.net/?retryWrites=true&w=majority";
 
 let connection
 const connectDB = async () => {
   if (!connection) {
     console.log("Connected to MongoDB")
-    connection = await mongoose.connect(url)
+    connection = await mongoose.connect(urlString)
     return connection
   }
   console.log("Connected to MongoDB")
