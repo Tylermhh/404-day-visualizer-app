93509ea87e6755b8bb3112a9791a0deb85a991ec
ChocoKnight <k.tann.578@gmail.com>
2024-03-11 19:39:50 -0700

    Adding drop down to choose between different ways to visualize data

--- packages/react-frontend/src/components/VisualizerPage.tsx
+++ packages/react-frontend/src/components/VisualizerPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import MainNav from "./Nav/MainNav";
+import { Container, Col, Dropdown, DropdownButton, Row, Stack } from 'react-bootstrap';
  
 function Visualizer() {
   return (
@@ -8,11 +9,28 @@ function Visualizer() {
       <MainNav 
         page = { "Visualizer" }
       />
-      <header className="App-header">
-        <p>Visualizer Page</p>
- 
-        <Link to="/">go back</Link>
-      </header>
+      <Stack gap = {3}>
+        <Container />
+        <Container>
+          <Row>
+            <Col sm={4}>
+              <DropdownButton id="dropdown-item-button" title="Dropdown button">
+                <Dropdown.Item eventKey="1">
+                  Action
+                </Dropdown.Item>
+                <Dropdown.Item eventKey="2">
+                  Another action
+                </Dropdown.Item>
+                <Dropdown.Item eventKey="3">
+                  Active Item
+                </Dropdown.Item>
+              </DropdownButton>
+            </Col>
+            <Col sm={8}>
+            </Col>
+          </Row>
+        </Container>
+      </Stack>
     </div>
   );
 }
