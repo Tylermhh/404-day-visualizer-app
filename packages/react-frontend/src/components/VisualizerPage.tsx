d53174bc4f66476d32a37f73c04dc05cd02a2b80
ChocoKnight <k.tann.578@gmail.com>
2024-03-12 02:44:43 -0700

    Added a way to choose specific categories

--- packages/react-frontend/src/components/VisualizerPage.tsx
+++ packages/react-frontend/src/components/VisualizerPage.tsx
@@ -1,12 +1,39 @@
 import React, { useState } from "react";
 import MainNav from "./Nav/MainNav";
-import { Container, Col, Form, Row, Stack } from 'react-bootstrap';
+import { Container, Col, Button, Form, Row, Stack } from 'react-bootstrap';
 import VisualizerNumberTasksPerCategoryPieChart from "./Chart/VisualizerNumberTasksPerCategoryPieChart"
 import {tempTasks, userCategories} from "./../TempData"
 
 function VisualizerType(value : any) {
+  const [category, setCategory] = useState('');
+  const handleSelectCategory=(e : any)=>{
+    console.log(e);
+    setCategory(e.target.value)
+  }
+
   if (value.value === '1') {
-    return <VisualizerNumberTasksPerCategoryPieChart tasks={tempTasks} categories={userCategories} />
+    return (
+      <Container className="d-flex justify-content-center">
+        <VisualizerNumberTasksPerCategoryPieChart tasks={tempTasks} categories={userCategories} />
+      </Container>
+    )
+  }
+  else if (value.value === '2'){
+    return (
+      <Container>
+        <Row>
+          <Col sm={4} />
+          <Col sm={4}>
+            <Form>
+              <Form.Select value={category} onChange={handleSelectCategory}>
+                {userCategories.map((categories, index) => (<option value={index}>{categories.name}</option>))}
+              </Form.Select>
+            </Form>
+          </Col>
+          <Col sm={4} />
+        </Row>
+      </Container>
+    )
   } else {
     return (
       <text>
@@ -15,14 +42,14 @@ function VisualizerType(value : any) {
     )
   }
 }
- 
+
 function Visualizer() {
   const [date, setDate] = useState(new Date());
 
-  const [value, setValue] = useState('');
-  const handleSelect=(e : any)=>{
+  const [visualizer, setVisualizer] = useState('');
+  const handleSelectVisualizer=(e : any)=>{
     console.log(e);
-    setValue(e.target.value)
+    setVisualizer(e.target.value)
   }
 
   return (
@@ -36,9 +63,9 @@ function Visualizer() {
           <Row>
             <Col sm={4}>
               <Form>
-                <Form.Select value={value} onChange={handleSelect}>
-                  <option value="1">Number of Tasks / Category</option>
-                  <option value="2">Hours / Task</option>
+                <Form.Select value={visualizer} onChange={handleSelectVisualizer}>
+                  <option value="1">Number of Tasks Per Category</option>
+                  <option value="2">Average Hours Spent Per Task</option>
                 </Form.Select>
               </Form>
             </Col>
@@ -53,13 +80,22 @@ function Visualizer() {
                 />
               </Form>
             </Col>
+            <Col sm={4}>
+              <Form>
+                <Form.Control
+                  type="date"
+                  name="endDate"
+                  placeholder="DateRange"
+                  value={date.toUTCString()}
+                  onChange={(e) => setDate(new Date(e.target.value))}
+                />
+              </Form>
+            </Col>
           </Row>
         </Container>
         <Container>
           <Row>
-            <Container className="d-flex justify-content-center">
-              <VisualizerType value={value}/>
-            </Container>
+            <VisualizerType value={visualizer}/>
           </Row>
         </Container>
       </Stack>
