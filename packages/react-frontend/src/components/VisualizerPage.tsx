be8b7e2aee9e0ce7064847ae57498c8fd4c3ab41
ChocoKnight <k.tann.578@gmail.com>
2024-03-12 00:18:05 -0700

    Working pie chart for tasks, hours, and percentages

--- packages/react-frontend/src/components/VisualizerPage.tsx
+++ packages/react-frontend/src/components/VisualizerPage.tsx
@@ -1,8 +1,30 @@
 import React, { useState } from "react";
 import MainNav from "./Nav/MainNav";
-import { Container, Col, Dropdown, DropdownButton, Row, Stack } from 'react-bootstrap';
+import { Container, Col, Dropdown, DropdownButton, Form, Row, Stack } from 'react-bootstrap';
+import VisualizerNumberTasksPerCategoryPieChart from "./Chart/VisualizerNumberTasksPerCategoryPieChart"
+import {tempTasks, userCategories} from "./../TempData"
+
+function VisualizerType(value : any) {
+  if (value.toString() === '1') {
+    return <VisualizerNumberTasksPerCategoryPieChart tasks={tempTasks} categories={userCategories} />
+  } else {
+    return (
+      <Container className="d-flex justify-content-center">
+        <VisualizerNumberTasksPerCategoryPieChart tasks={tempTasks} categories={userCategories} />;
+      </Container>
+    )
+  }
+}
  
 function Visualizer() {
+  const [date, setDate] = useState(new Date());
+
+  const [value, setValue] = useState('');
+  const handleSelect=(e : any)=>{
+    console.log(e);
+    setValue(e.toString())
+  }
+
   return (
     <div className="App">
       <MainNav 
@@ -13,8 +35,8 @@ function Visualizer() {
         <Container>
           <Row>
             <Col sm={4}>
-              <DropdownButton id="dropdown-item-button" title="Visualization Method">
-                <Dropdown.Item eventKey="1" as="button">
+              <DropdownButton id="dropdown-item-button" title="Visualization Method" onSelect={handleSelect}>
+                <Dropdown.Item eventKey="1">
                   Number of Tasks / Category
                 </Dropdown.Item>
                 <Dropdown.Item eventKey="2" as="button">
@@ -22,8 +44,22 @@ function Visualizer() {
                 </Dropdown.Item>
               </DropdownButton>
             </Col>
-            <Col sm={8}>
+            <Col sm={4}>
+              <Form>
+                <Form.Control
+                  type="date"
+                  name="startDate"
+                  placeholder="DateRange"
+                  value={date.toUTCString()}
+                  onChange={(e) => setDate(new Date(e.target.value))}
+                />
+              </Form>
             </Col>
+          </Row>
+        </Container>
+        <Container>
+          <Row>
+            <VisualizerType value={value}/>
           </Row>
         </Container>
       </Stack>
