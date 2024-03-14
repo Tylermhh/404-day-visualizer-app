dbee7af8d20a3e1b433228696ec5abf2296bcb0a
Tylermhh <tyler.hlaing@gmail.com>
2024-03-14 01:27:13 -0700

    fixed displaying dates

--- packages/react-frontend/src/components/Modals/TaskDetailsModal.tsx
+++ packages/react-frontend/src/components/Modals/TaskDetailsModal.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
-import { ITask } from "../../types/types";
+import { IDateEntry, ITask } from "../../types/types";
 
 const TaskDetailsModal: React.FC<{
   task: ITask;
@@ -9,6 +9,20 @@ const TaskDetailsModal: React.FC<{
 
     const task = input.task;
   const [isModalVisible, setIsModalVisible] = useState(false);
+
+  // const formatter = new Intl.DateTimeFormat("en-US", {
+  //   day: "2-digit",
+  //   month: "2-digit",
+  //   year: "numeric",
+  // });
+  // const createdAtFormat = formatter.format(task.createdAt);
+  const createdString = task.createdAt.toString();
+  const new_created = new Date(createdString);
+  const new_created_string = new_created.toDateString();
+
+  const deadlineString = task.createdAt.toString();
+  const new_deadline = new Date(deadlineString);
+  const new_deadline_string = new_deadline.toDateString();
   
   const toggleModal = () => {
     setIsModalVisible(!isModalVisible);
@@ -30,9 +44,20 @@ const TaskDetailsModal: React.FC<{
             <p>{`Category : ${task.category}`}</p>
             <p>{`Priority : ${task.priority}`}</p>
             <p>{`Completed? : ${task.done}`}</p>
-            <p>{`Deadline : ${task.deadline}`}</p>
-            <p>{`Date Created : ${task.createdAt}`}</p>
-            <p>{`Dates Updated : ${task.datesUpdated}`}</p>  
+            <p>{`Deadline : ${new_deadline_string}`}</p>
+            <p>{`Date Created : ${new_created_string}`}</p>
+            <p>{`Dates Updated :`} </p> 
+            <p>{`[`} </p> 
+             {task.datesUpdated.map((elem: IDateEntry, index: any) => 
+            {
+              const updateString = elem.date.toString();
+              const new_update = new Date(updateString);
+              const new_update_string = new_update.toDateString();
+              return (
+                <p>{`${new_update_string} => ${elem.hours} hours`}</p>
+              );
+            })}
+            <p>{`]`} </p> 
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={toggleModal}>
