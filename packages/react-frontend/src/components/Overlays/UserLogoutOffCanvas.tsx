b6d814494984430f7905552c3c9a964f80324174
ChocoKnight <k.tann.578@gmail.com>
2024-03-14 05:29:33 -0700

    add rectangle to offcanvas color cell

--- packages/react-frontend/src/components/Overlays/UserLogoutOffCanvas.tsx
+++ packages/react-frontend/src/components/Overlays/UserLogoutOffCanvas.tsx
@@ -3,6 +3,7 @@ import {Button, Stack, Offcanvas} from 'react-bootstrap';
 import { getUser } from "./../../api/UserHooks";
 import { userID } from "./../User";
 import { Category } from "./../../types/types"
+import UserCategoryTable from "./../Table/UserCategoryTable";
 
 const UserLogoutOffCanvas: React.FC<{}> = (input) => {    
     const [show, setShow] = useState(false);
@@ -38,12 +39,7 @@ const UserLogoutOffCanvas: React.FC<{}> = (input) => {
                 </Offcanvas.Header>
                 <Offcanvas.Body>
                     <Stack gap={2}>
-                        <text>
-                            User Categories
-                        </text>
-                        <text>
-                            {JSON.stringify(categories)}
-                        </text>
+                        <UserCategoryTable categories={categories}/>
                         <Button variant="primary">
                             Logout
                         </Button>
---
+++ packages/react-frontend/src/components/Table/UserCategoryTable.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import styles from "./Table.module.css";
+import { Col, Row, Table} from 'react-bootstrap';
+import { Category } from "./../../types/types";
+
+const TableTitle: React.FC<{}> = () => {
+  return (
+    <div>
+      <Row>
+        <Col sm={12}>
+          <text>
+            User Categories
+          </text>
+        </Col>
+      </Row>
+    </div>
+  )
+}
+
+const TableBody: React.FC<{categories : Category[]}> = (input) => {
+  const rows = input.categories.map((row: any, index: any) => {
+      return (
+        <tr>
+            <td>{row.name}</td>
+            <div className = {styles['color-box']}>
+                <td>
+                    <div className = {styles['blue-box']}>
+                        <span> </span> 
+                    </div>
+                </td>
+            </div>
+        </tr>
+      );
+  });
+  
+  return (
+    <div className = {styles['table-responsive']}>
+      <Table striped hover>
+        <thead>
+          <tr>
+            <th>Category</th>
+            <th>Color</th>
+          </tr>
+        </thead>
+        <tbody>
+          { rows }
+        </tbody>
+      </Table>
+    </div>
+  );
+}
+
+const UserCategoryTable: React.FC<{categories : Category[]}> = (input) => {
+  return (
+    <div>
+      <TableTitle />
+      <TableBody
+        categories = {input.categories}
+      />
+    </div>
+  )
+}
+
+export default UserCategoryTable
