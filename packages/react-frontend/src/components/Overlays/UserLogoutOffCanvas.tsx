341e623779aecdf853967d54de25ebaff6eb5769
ChocoKnight <k.tann.578@gmail.com>
2024-03-14 04:40:54 -0700

    got user cateogireis as json

--- packages/react-frontend/src/components/Overlays/UserLogoutOffCanvas.tsx
+++ packages/react-frontend/src/components/Overlays/UserLogoutOffCanvas.tsx
@@ -1,36 +1,56 @@
-import { useState } from 'react';
+import React, { useState, useEffect } from "react";
 import {Button, Stack, Offcanvas} from 'react-bootstrap';
+import { getUser } from "./../../api/UserHooks";
+import { userID } from "./../User";
+import { Category } from "./../../types/types"
 
 const UserLogoutOffCanvas: React.FC<{}> = (input) => {    
     const [show, setShow] = useState(false);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+    const [categories, setCategories] = useState<Category[]>([]);
 
-  return (
-    <>
-      <Button variant="light" onClick={handleShow}>
-        User
-      </Button>
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
-      <Offcanvas show={show} onHide={handleClose} placement='end'>
-        <Offcanvas.Header closeButton>
-            <Offcanvas.Title>
-                User Profile
-            </Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body>
-            <Stack gap={2}>
-                <text>
-                    user stuff
-                </text>
-                <Button variant="primary">
-                    Logout
-                </Button>
-            </Stack>
-        </Offcanvas.Body>
-      </Offcanvas>
-    </>
+    useEffect(() => {
+        getUser(userID)
+        .then(res => {
+            res.json().then(data => {
+                setCategories(data.categories)
+            });
+        })
+        .catch(err => {
+            console.error(err);
+        });
+    });
+
+    return (
+        <>
+            <Button variant="light" onClick={handleShow}>
+                User
+            </Button>
+
+            <Offcanvas show={show} onHide={handleClose} placement='end'>
+                <Offcanvas.Header closeButton>
+                    <Offcanvas.Title>
+                        User Profile
+                    </Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                    <Stack gap={2}>
+                        <text>
+                            User Categories
+                        </text>
+                        <text>
+                            {JSON.stringify(categories)}
+                        </text>
+                        <Button variant="primary">
+                            Logout
+                        </Button>
+                    </Stack>
+                </Offcanvas.Body>
+            </Offcanvas>
+        </>
   );
 }
 
