ab019362a3197b41edc9dc7bbdc56dddd43d13c9
ChocoKnight <k.tann.578@gmail.com>
2024-03-14 11:10:18 -0700

    Got logout to work

--- packages/react-frontend/src/components/Overlays/UserLogoutOffCanvas.tsx
+++ packages/react-frontend/src/components/Overlays/UserLogoutOffCanvas.tsx
@@ -1,14 +1,23 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
 import {Button, Stack, Offcanvas} from 'react-bootstrap';
 import { getUser } from "./../../api/UserHooks";
 import { Category } from "./../../types/types"
 import UserCategoryTable from "./../Table/UserCategoryTable";
+import { useAuth } from './../../context/AuthContext';
 
 const UserLogoutOffCanvas: React.FC<{}> = (input) => {    
     const [show, setShow] = useState(false);
 
     const [username, setUsername] = useState("");
     const [categories, setCategories] = useState<Category[]>([]);
+
+    const { logout } = useAuth();
+    const navigate = useNavigate();
+    const logoutHandle = () => {
+        logout();
+        navigate('/');
+    }
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
@@ -44,7 +53,7 @@ const UserLogoutOffCanvas: React.FC<{}> = (input) => {
                             Username: {username}
                         </text>
                         <UserCategoryTable categories={categories}/>
-                        <Button variant="primary">
+                        <Button variant="primary" onClick={logoutHandle}>
                             Logout
                         </Button>
                     </Stack>
