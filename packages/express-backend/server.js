87dff56e0698e2834a2c82bc311ae842420716f7
Sean Estrella <sean.estrella2002@gmail.com>
2024-03-13 17:06:55 -0700

    Refactor server setup for clarity and adjust CORS configuration
    - Reorder import statements to group similar types (ES6 import syntax consistency).
    - Add explicit comments to highlight CORS setup options, facilitating easy switches between all-requests and specific-origin configurations.
    - Remove conditional logging for server listening message, simplifying startup log output.
    - Reorganize middleware usage to ensure `cors` and `express.json()` are applied before route definitions, enhancing readability and maintaining middleware execution order.

--- packages/express-backend/server.js
+++ packages/express-backend/server.js
@@ -1,30 +1,32 @@
 import express from "express";
+import cors from 'cors'; // Change this line to use ES6 import
 import taskRoutes from "./routes/task-routes.js";
 import userRoutes from "./routes/user-routes.js";
 import authRoutes from "./routes/auth-routes.js";
-import cors from "cors";
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 
-dotenv.config({ path: "../../.env" });
+dotenv.config({ path: '../../.env' });
 
 const port = 8000;
 const app = express();
+
+// Enable All CORS Requests for development purposes
+app.use(cors());
+
+// For specific origin (e.g., your frontend in development)
+// app.use(cors({ origin: 'http://localhost:3000' }));
 
 app.get("/", (req, res) => {
   res.send("Todo: Project Name");
 });
 
-app.use(cors());
 app.use(express.json());
 
-app.listen(process.env.PORT || port, () => {
-  if (process.env.PORT) {
-    console.log("REST API is listening.");
-  } else {
-    console.log(`Example app listening at http://localhost:${port}`);
-  }
-});
-
+// Define routes after cors middleware
 app.use("/task", taskRoutes);
 app.use("/users", userRoutes);
 app.use("/auth", authRoutes);
+
+app.listen(process.env.PORT || port, () => {
+  console.log("REST API is listening.");
+});
